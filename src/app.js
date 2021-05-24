d343f37841a91fe82351d50defb4eb8e3315a4f1
r8ch <81969279+r8ch@users.noreply.github.com>
2021-05-24 16:31:53 +0200

    Updated api call

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
-function formatDate(timestamp){
-let date = new Date(timestamp);
-let hours = date.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = date.getMinutes();
-if (minutes <10){
-    minutes = `0${minutes}`
-}
-let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -24,12 +32,12 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  let dateElement =document.querySelector("#date");
-  dateElement.innerHTML = formatDate (response.data.dt*1000)
-
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "01ef729c80c15fd2961d6c2c3b6616c8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Roskilde&appid=${apiKey}&units=metric`;
+let city = "Roskilde";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
