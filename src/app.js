d83ba37f399194f89e609c12a91f8a1effcd7dec
r8ch <81969279+r8ch@users.noreply.github.com>
2021-05-24 16:28:54 +0200

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes <10){
+    minutes = `0${minutes}`
+}
+let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -7,8 +22,11 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
-let windElement = document.querySelector("#wind");
-windElement.innerHTML = Math.round(response.data.wind.speed);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement =document.querySelector("#date");
+  dateElement.innerHTML = formatDate (response.data.dt*1000)
+
 }
 
 let apiKey = "01ef729c80c15fd2961d6c2c3b6616c8";
